b55788af9c142590d78e7b173dc4b24fdf1316b4
oliverpa-dev <o.paszkiewicz@student.fontys.nl>
2021-02-24 17:25:41 +0100

    Finished version of the Hackman
    It was fun

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # This is the first project that we have created with our new teacher.
-## In this file I am following the code what was written during the first class.
+## In this file I am following the code that was written during the first class.
--- hackman.js
+++ hackman.js
@@ -1,38 +1,132 @@
-console.log("The Hackman game has started !!")
+console.log("Welcome to the brand New Gamae that is HackMan");
 
-// 1=wall, 2=background, 3=dot, 4=hackman
+// Creating a score for everysingle dot that the hackman has eaten
+var dotScore = 10;
+var score = 0;
+
+// Create an object that contains the position of hackman
+var hacmanPos = {
+    x: 4,
+    y: 5
+}
+
+// Create a two dimension array that represents the map
+// It is consits of rows and columns
+// If it is 1=wall, 2=background, 3=dot
 var map = [
-    [1, 1, 1, 1, 1],
-    [1, 3, 3, 3, 1],
-    [1, 3, 4, 3, 1],
-    [1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 3, 3, 3, 3, 3, 3, 1],
+    [1, 3, 3, 3, 3, 3, 3, 1],
+    [1, 3, 3, 3, 3, 3, 3, 1],
+    [1, 3, 3, 3, 3, 3, 3, 1],
+    [1, 3, 3, 3, 4, 3, 3, 1],
+    [1, 3, 3, 3, 3, 3, 3, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-// CREATE A FUNCTION TO DRAW ENTIRE BOARD
-
+//Create a Function to draw entire board
 function drawBoard() {
 
-    var htmlBoard = document.getElementById("board")
+    // "board" variable refers to the div that we have in html file(We take this by using document(html) + getElementById)
+    var htmlBoard = document.getElementById("board");
+    // Board is empty
+    htmlBoard.innerHTML = "";
+    // Instead of 10 we take the length of the map
+    // Because we have to dimensional array, we have to create one more loop
+    // The firs loop is for x and the second one is for y
+    //! Here it takes the length of the map
     for (let y = 0; y < map.length; y++) {
+        //! Here it takes the length of the y direction
         for (let x = 0; x < map[y].length; x++) {
-
-            //add a div
+            // We want to check the value of the map
+            // ! "===" - checks if they are the same value as well as type
             if (map[y][x] === 1) {
-                htmlBoard.innerHTML += "<div class= 'wall tile'></div>"
+                // add a div
+                // += - add to it
+                // remember to use single quotes because double quotes does not work withing double qoutes
+                htmlBoard.innerHTML += "<div class='wall tile'></div>"
             }
             if (map[y][x] === 2) {
-                htmlBoard.innerHTML += "<div class= 'background tile'></div>"
+                // add a div
+                // += - add to it
+                // remember to use single quotes because double quotes does not work withing double qoutes
+                htmlBoard.innerHTML += "<div class='background tile'></div>"
             }
             if (map[y][x] === 3) {
-                htmlBoard.innerHTML += "<div class= 'dot tile'></div>"
+                // add a div
+                // += - add to it
+                // remember to use single quotes because double quotes does not work withing double qoutes
+                htmlBoard.innerHTML += "<div class='dot tile'></div>"
             }
             if (map[y][x] === 4) {
-                htmlBoard.innerHTML += "<div class= 'hacman tile'></div>"
+                // add a div
+                // += - add to it
+                // remember to use single quotes because double quotes does not work withing double qoutes
+                htmlBoard.innerHTML += "<div class='hacman tile'></div>"
             }
-
         }
+        // What is does it will separate the div so they will be equal (after the row is done)
         htmlBoard.innerHTML += "<br>"
     }
+    var wScore = document.getElementById("score");
+    wScore.innerText = "The Score is " + score;
+}
+
+// This is an event listener that listens if there was any key pressed
+document.onkeydown = function (event) {
+    // creating a variable for keyPressed
+    var keyPressed = event.key;
+
+    switch (keyPressed) {
+        case "ArrowUp":
+            // creating the condition that checks if hackman is inside the wall
+            if (map[hacmanPos.y - 1][hacmanPos.x] != 1) {
+                if (map[hacmanPos.y - 1][hacmanPos.x] == 3) {
+                    score += dotScore;
+                }
+                // Leaves the background empty
+                map[hacmanPos.y][hacmanPos.x] = 2;
+                // new position of the hackman
+                hacmanPos.y -= 1;
+                // shows the hackman at the new position
+                map[hacmanPos.y][hacmanPos.x] = 4;
+            }
+            break;
+        case "ArrowDown":
+            if (map[hacmanPos.y + 1][hacmanPos.x] != 1) {
+                if (map[hacmanPos.y + 1][hacmanPos.x] == 3) {
+                    score += dotScore;
+                }
+                map[hacmanPos.y][hacmanPos.x] = 2;
+                hacmanPos.y += 1;
+                map[hacmanPos.y][hacmanPos.x] = 4;
+            }
+            break;
+        case "ArrowLeft":
+            if (map[hacmanPos.x - 1][hacmanPos.y] != 1) {
+                if (map[hacmanPos.x - 1][hacmanPos.y] == 3) {
+                    score += dotScore;
+                }
+                map[hacmanPos.y][hacmanPos.x] = 2;
+                hacmanPos.x -= 1;
+                map[hacmanPos.y][hacmanPos.x] = 4;
+            }
+            break;
+        case "ArrowRight":
+            if (map[hacmanPos.x + 1][hacmanPos.y] != 1) {
+                if (map[hacmanPos.x + 1][hacmanPos.y] == 3) {
+                    score += dotScore;
+                }
+                map[hacmanPos.y][hacmanPos.x] = 2;
+                hacmanPos.x += 1;
+                map[hacmanPos.y][hacmanPos.x] = 4;
+            }
+            break;
+
+        default:
+            break;
+    }
+    drawBoard();
 }
 
 drawBoard();
